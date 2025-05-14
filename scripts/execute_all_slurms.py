33a9ce9ce935f152910fc1f88ade5949400e1770
Alexey Yermakov <yyexela@proton.me>
2025-05-14 15:47:57 -0700

    Added 0.25 second interval between submitted job

--- scripts/execute_all_slurms.py
+++ scripts/execute_all_slurms.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pathlib import Path
 
 top_dir = Path(__file__).parent.parent
@@ -7,3 +8,4 @@ slurm_dir = top_dir / 'slurms'
 for slurm_file in slurm_dir.glob('*.slurm'):
     print(f"Submitting {slurm_file}")
     os.system(f'sbatch {slurm_file}')
+    time.sleep(0.25)
