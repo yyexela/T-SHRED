a1d54a9806aa623a3af7099dade3d5c4a6d713c3
Alexey Yermakov <yyexela@proton.me>
2025-05-15 16:42:14 -0700

    Updated scripts

--- scripts/execute_all_slurms.py
+++ scripts/execute_all_slurms.py
@@ -5,7 +5,12 @@ from pathlib import Path
 top_dir = Path(__file__).parent.parent
 slurm_dir = top_dir / 'slurms'
 
-for slurm_file in slurm_dir.glob('*.slurm'):
-    print(f"Submitting {slurm_file}")
-    os.system(f'sbatch {slurm_file}')
-    time.sleep(0.25)
+def main(args=None):
+    for slurm_file in slurm_dir.glob('*.slurm'):
+        print(f"Submitting {slurm_file}")
+        os.system(f'sbatch {slurm_file}')
+        time.sleep(0.25)
+    
+if __name__ == '__main__':
+    main()       
+        
