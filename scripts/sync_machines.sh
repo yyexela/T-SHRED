69e109befbb855560943d1f999a16cd789146118
Alexey Y <alexeyy@uw.edu>
2025-09-26 09:24:42 -0700

    Ran hyperparameter tuning between SL GRU and SASL-T

--- scripts/sync_machines.sh
+++ scripts/sync_machines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if hostname is kutz-lambda-matrix
+# Check hostname to set up target
 if [ "$(hostname)" == "kutz-lambda-matrix" ]; then
     target_machine="vector"
 else
@@ -8,15 +8,23 @@ else
 fi
 
 for dir in "results" "scripts" "src" "notebooks" "configs" "bash" "pickles" ".vscode"; do
+    # Sync to target machine
+    #echo "Running: rsync -az \"/home/alexey/Git/T-SHRED/$dir\" $target_machine:\"/home/alexey/Git/T-SHRED/\""
     rsync -az "/home/alexey/Git/T-SHRED/$dir" $target_machine:"/home/alexey/Git/T-SHRED/"
-    #echo "rsync -az \"/home/alexey/Git/T-SHRED/$dir\" $target_machine:\"/home/alexey/Git/T-SHRED/\""
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to sync $dir to $target_machine"
-    fi
+    to_result=$?
+    
+    # Sync from target machine
+    #echo "Running: rsync -az $target_machine:\"/home/alexey/Git/T-SHRED/$dir\" \"/home/alexey/Git/T-SHRED/\""
     rsync -az $target_machine:"/home/alexey/Git/T-SHRED/$dir" "/home/alexey/Git/T-SHRED/"
-    #echo "rsync -az $target_machine:\"/home/alexey/Git/T-SHRED/$dir\" \"/home/alexey/Git/T-SHRED/\""
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to sync $dir from $target_machine"
+    from_result=$?
+    
+    # Check results and print appropriate message
+    if [ $to_result -ne 0 ]; then
+        echo "Error: Failed to sync '$dir/' to $target_machine"
+    elif [ $from_result -ne 0 ]; then
+        echo "Error: Failed to sync '$dir/' from $target_machine"
+    else
+        echo "Success: Synced '$dir/' with $target_machine"
     fi
 done
 
